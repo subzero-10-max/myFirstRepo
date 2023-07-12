f1b44f7268e6e44988d868c79dacb184cbad4e25
subzero-10-max <69166784+subzero-10-max@users.noreply.github.com>
2023-07-13 00:01:38 +0500

    Update backend.tf

--- code/Config/backend.tf
+++ code/Config/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name = "talha_terraform_backend"
+    storage_account_name = "talhaterraformbackend10"
+    container_name = "terraform-backend"
+    key = "terraform.tfstate"
+  }
 }
